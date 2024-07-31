6e8c49e3675c8bdb85f339d8b21c4a094b5b807a
Mayank Tyagi <mayank.morta1209@gmail.com>
2024-07-31 14:30:19 +0530

    summer internship

---
+++ day2.py
@@ -0,0 +1,32 @@
+# var=20
+# print(var)
+# print(type(var))
+
+
+# int var =10;
+# int  ===>data type
+# _ =25
+# print (_)
+
+
+# st ="mr jack @1209"
+# print(st)
+
+# print(type(st))
+
+# st='mrjack'
+# print(st)
+
+# st='BFIYVUWGQFO'
+# print(st[5])
+
+
+# st = "upflairs pvt ltd jaipur rajasthan"
+# # print(st[17:24])
+# print(st.count("j"))
+    #   print(st.count("H"))
+st = "python is easy to learn"
+# print(st.endswith("easy"))
+#  print(st.endswith("python"))
+# print(st.startswith("learn"))
+print(st.endswith("python"))
